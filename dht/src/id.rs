40e55fc90240a158a46c4c1b158765536f0b1e9d
Gurwinder Singh <vargwin@gmail.com>
2020-03-21 16:14:35 +0530

    Begin DHT

--- dht/src/id.rs
+++ dht/src/id.rs
@@ -1,11 +1,56 @@
+use data_encoding::HEXUPPER;
+use rand::Rng;
 use std::ops::BitXor;
 
-type Bytes = [u8; 20];
-
-#[derive(Clone, Debug, Default, PartialEq, PartialOrd)]
-pub struct NodeId(Bytes);
+#[derive(Clone, Debug, Default, PartialEq, PartialOrd, Eq, Ord)]
+pub struct NodeId([u8; NodeId::LEN]);
 
 impl NodeId {
+    pub const LEN: usize = 20;
+
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn of_byte(b: u8) -> Self {
+        Self([b; Self::LEN])
+    }
+
+    pub fn random() -> Self {
+        let mut n = Self::new();
+        rand::thread_rng().fill(&mut n.0[..]);
+        n
+    }
+
+    pub fn decode_hex(hex: &[u8]) -> Result<Self, &'static str> {
+        let mut n = Self::new();
+
+        let len = HEXUPPER
+            .decode_len(hex.len())
+            .map_err(|_| "Invalid hex length for node ID")?;
+
+        if len != n.0.len() {
+            return Err("Invalid hex for node ID");
+        }
+
+        HEXUPPER
+            .decode_mut(hex, &mut n.0)
+            .map_err(|_| "Unable to parse hex string")?;
+
+        Ok(n)
+    }
+
+    pub fn encode_hex(&self, buf: &mut [u8]) -> Result<(), &'static str> {
+        let len = HEXUPPER.encode_len(self.0.len());
+
+        if len != buf.len() {
+            return Err("Invalid hex for node ID");
+        }
+
+        HEXUPPER.encode_mut(&self.0, buf);
+        Ok(())
+    }
+
     pub fn dist(&self, other: &Self) -> Self {
         self ^ other
     }
@@ -16,22 +61,22 @@ impl NodeId {
         lhs < rhs
     }
 
-    pub fn dist_exp(&self, other: &Self) -> u32 {
+    pub fn dist_exp(&self, other: &Self) -> usize {
         160 - self.dist(other).leading_zeros()
     }
 
-    pub fn min_dist_exp(&self, ids: &[Self]) -> u32 {
+    pub fn min_dist_exp(&self, ids: &[Self]) -> usize {
         debug_assert_ne!(ids.len(), 0);
         ids.iter().map(|id| self.dist_exp(id)).min().unwrap_or(160)
     }
 
-    pub fn leading_zeros(&self) -> u32 {
+    pub fn leading_zeros(&self) -> usize {
         let mut n = 0;
         for &c in self.0.iter() {
             if c == 0 {
                 n += 8;
             } else {
-                n += c.leading_zeros();
+                n += c.leading_zeros() as usize;
                 break;
             }
         }
@@ -43,10 +88,54 @@ impl BitXor<&NodeId> for &NodeId {
     type Output = NodeId;
 
     fn bitxor(self, other: &NodeId) -> NodeId {
-        let mut data = Bytes::default();
-        data.iter_mut()
+        let mut n = NodeId::default();
+        n.0.iter_mut()
             .zip(self.0.iter().zip(other.0.iter()))
             .for_each(|(a, (b, c))| *a = b ^ c);
-        NodeId(data)
+        n
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_hex() {
+        let h = b"3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F";
+        let n = NodeId::decode_hex(h).unwrap();
+        assert_eq!(NodeId::of_byte(0x3F), n);
+    }
+
+    #[test]
+    fn encode_hex() {
+        let n = NodeId::of_byte(0x3F);
+        let mut buf = [0; NodeId::LEN * 2];
+        n.encode_hex(&mut buf).unwrap();
+        assert_eq!(b"3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F3F"[..], buf[..]);
+    }
+
+    #[test]
+    fn sort_order() {
+        let mut a = [NodeId::of_byte(0), NodeId::of_byte(3), NodeId::of_byte(1)];
+        a.sort();
+        assert_eq!(
+            [NodeId::of_byte(0), NodeId::of_byte(1), NodeId::of_byte(3)],
+            a
+        );
+    }
+
+    #[test]
+    fn xor() {
+        let a = NodeId::of_byte(0b0000_0101);
+        let b = NodeId::of_byte(0b1100_0100);
+        let c = &a ^ &b;
+        assert_eq!(NodeId::of_byte(0b1100_0001), c);
+    }
+
+    #[test]
+    fn leading_zeros() {
+        let a = NodeId::of_byte(0b0000_1000);
+        assert_eq!(4, a.leading_zeros());
     }
 }
--- dht/src/table.rs
+++ dht/src/table.rs
@@ -1,4 +1,8 @@
 use crate::entry::NodeEntry;
+use crate::id::NodeId;
+use std::collections::VecDeque;
+
+const BUCKET_SIZE: usize = 20;
 
 pub type Bucket = Vec<NodeEntry>;
 
@@ -7,4 +11,39 @@ pub struct RoutingNode {
     pub live: Bucket,
 }
 
-pub struct RoutingTable {}
+pub struct Contact {
+    id: NodeId,
+}
+
+pub struct RoutingTable {
+    node: NodeId,
+    buckets: VecDeque<VecDeque<Contact>>,
+}
+
+impl RoutingTable {
+    pub fn new(node: NodeId) -> Self {
+        let mut buckets = VecDeque::with_capacity(NodeId::LEN * 8);
+        for _ in 0..buckets.capacity() {
+            buckets.push_back(VecDeque::new());
+        }
+        Self { node, buckets }
+    }
+
+    pub fn update(&mut self, contact: Contact) {
+        let table_node = &self.node;
+        let bucket_no = (table_node ^ &contact.id).leading_zeros();
+        let bucket = &mut self.buckets[bucket_no];
+
+        let maybe_idx = bucket.iter().position(|c| c.id == contact.id);
+
+        if let Some(idx) = maybe_idx {
+            let contact = bucket.remove(idx).unwrap();
+            bucket.push_front(contact);
+        } else if bucket.len() < BUCKET_SIZE {
+            bucket.push_front(contact);
+        } else {
+            // TODO: Handle insertion when the list is full by evicting old elements if
+            // they don't respond to a ping
+        }
+    }
+}
