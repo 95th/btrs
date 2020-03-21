475864927ea2b20b606ece81c3c205e8755ddc6f
Gurwinder Singh <vargwin@gmail.com>
2020-03-21 18:23:59 +0530

    DHT: Find closest node

--- dht/src/table.rs
+++ dht/src/table.rs
@@ -30,8 +30,7 @@ impl RoutingTable {
     }
 
     pub fn update(&mut self, contact: Contact) {
-        let table_node = &self.node;
-        let bucket_no = (table_node ^ &contact.id).leading_zeros();
+        let bucket_no = (&self.node ^ &contact.id).leading_zeros();
         let bucket = &mut self.buckets[bucket_no];
 
         let maybe_idx = bucket.iter().position(|c| c.id == contact.id);
@@ -44,6 +43,41 @@ impl RoutingTable {
         } else {
             // TODO: Handle insertion when the list is full by evicting old elements if
             // they don't respond to a ping
+        }
+    }
+
+    pub fn find_closest(&self, target: &NodeId, count: usize) -> Vec<&Contact> {
+        let mut contacts = Vec::with_capacity(count);
+
+        let table_node = &self.node;
+        let bucket_no = (table_node ^ &target).leading_zeros();
+
+        self.add_contacts(&mut contacts, bucket_no, count);
+
+        let mut i = 1;
+        while contacts.len() < count && (i < bucket_no || bucket_no + i < self.buckets.len()) {
+            if i < bucket_no {
+                self.add_contacts(&mut contacts, bucket_no - i, count);
+            }
+
+            if bucket_no + i < self.buckets.len() {
+                self.add_contacts(&mut contacts, bucket_no + i, count);
+            }
+
+            i += 1;
+        }
+
+        contacts.sort_unstable_by_key(|c| &c.id ^ target);
+        contacts
+    }
+
+    fn add_contacts<'a>(&'a self, contacts: &mut Vec<&'a Contact>, bucket_no: usize, count: usize) {
+        for c in &self.buckets[bucket_no] {
+            if contacts.len() < count {
+                contacts.push(c);
+            } else {
+                break;
+            }
         }
     }
 }
