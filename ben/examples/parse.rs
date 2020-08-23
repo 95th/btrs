611aca83fea83e6ee58ac4d0520c9e5bca867653
Gurwinder Singh <vargwin@gmail.com>
2020-08-23 13:29:21 +0530

    Update btrs

--- ben/examples/parse.rs
+++
@@ -1,9 +0,0 @@
-use ben::decode::Dict;
-use ben::Parser;
-
-fn main() {
-    let s = b"d1:a1:b1:ci1e1:x1:y1:dde1:fle1:g1:he";
-    let mut parser = Parser::new();
-    let dict = parser.parse::<Dict>(s).unwrap();
-    println!("{:#?}", dict);
-}
