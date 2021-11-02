32961c036a147674fa9c5ea2ac1755322b34dadb
Gurwinder Singh <vargwin@gmail.com>
2021-11-02 16:57:17 +0530

    Use BytesMut for parsing

--- client-proto/src/msg.rs
+++ client-proto/src/msg.rs
@@ -1,4 +1,4 @@
-use bytes::Buf;
+use bytes::{Buf, BytesMut};
 
 pub const CHOKE: u8 = 0;
 pub const UNCHOKE: u8 = 1;
@@ -22,6 +22,7 @@ pub fn packet_header_len(id: u8) -> usize {
 
 #[derive(Debug, PartialEq)]
 pub enum Packet {
+    Keepalive,
     Choke,
     Unchoke,
     Interested,
@@ -36,39 +37,76 @@ pub enum Packet {
 }
 
 impl Packet {
-    pub fn read(len: u32, id: u8, data: &mut impl Buf) -> Packet {
-        match id {
+    pub fn read(data: &mut BytesMut) -> Result<Self, usize> {
+        let mut b = &data[..];
+
+        check_data(&b, 4)?;
+        let len = b.get_u32();
+
+        if len == 0 {
+            data.advance(4);
+            return Ok(Packet::Keepalive);
+        }
+
+        check_data(&b, 1)?;
+        let id = b.get_u8();
+
+        let packet = match id {
             CHOKE => Packet::Choke,
             UNCHOKE => Packet::Unchoke,
             INTERESTED => Packet::Interested,
             NOT_INTERESTED => Packet::NotInterested,
-            HAVE => Packet::Have {
-                index: data.get_u32(),
-            },
+            HAVE => {
+                check_data(&b, 4)?;
+                Packet::Have { index: b.get_u32() }
+            }
             BITFIELD => Packet::Bitfield { len: len - 1 },
-            REQUEST => Packet::Request {
-                index: data.get_u32(),
-                begin: data.get_u32(),
-                len: data.get_u32(),
-            },
-            PIECE => Packet::Piece {
-                index: data.get_u32(),
-                begin: data.get_u32(),
-                len: len - 9,
-            },
-            CANCEL => Packet::Cancel {
-                index: data.get_u32(),
-                begin: data.get_u32(),
-                len: data.get_u32(),
-            },
+            REQUEST => {
+                check_data(&b, 12)?;
+                Packet::Request {
+                    index: b.get_u32(),
+                    begin: b.get_u32(),
+                    len: b.get_u32(),
+                }
+            }
+            PIECE => {
+                check_data(&b, 8)?;
+                Packet::Piece {
+                    index: b.get_u32(),
+                    begin: b.get_u32(),
+                    len: len - 9,
+                }
+            }
+            CANCEL => {
+                check_data(&b, 12)?;
+                Packet::Cancel {
+                    index: b.get_u32(),
+                    begin: b.get_u32(),
+                    len: b.get_u32(),
+                }
+            }
             EXTENDED => Packet::Extended { len: len - 1 },
             _ => Packet::Unknown { id, len: len - 1 },
-        }
+        };
+
+        let read = data.remaining() - b.remaining();
+        data.advance(read);
+
+        Ok(packet)
     }
+}
+
+fn check_data(b: &impl Buf, len: usize) -> Result<(), usize> {
+    if b.remaining() < len {
+        return Err(len - b.remaining());
+    }
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use bytes::BufMut;
+
     use crate::conn::Connection;
 
     use super::*;
@@ -77,11 +115,9 @@ mod tests {
     fn parse_piece() {
         let mut c = Connection::new();
         c.send_piece(1, 2, &[]);
-        let mut b = &c.get_send_buf()[..];
+        let mut b = c.get_send_buf()[..].into();
 
-        let len = b.get_u32();
-        let id = b.get_u8();
-        let packet = Packet::read(len, id, &mut b);
+        let packet = Packet::read(&mut b).unwrap();
 
         assert_eq!(
             packet,
@@ -94,15 +130,22 @@ mod tests {
     }
 
     #[test]
+    fn parse_piece_incomplete() {
+        let mut b = BytesMut::new();
+        b.put_u32(15);
+        b.put_u8(PIECE);
+        b.put_u32(2);
+
+        let err = Packet::read(&mut b).unwrap_err();
+        assert_eq!(err, 4);
+    }
+
+    #[test]
     fn parse_choke() {
         let mut c = Connection::new();
         c.send_choke();
-        let mut b = &c.get_send_buf()[..];
-
-        let len = b.get_u32();
-        let id = b.get_u8();
-        let packet = Packet::read(len, id, &mut b);
-
+        let mut b = c.get_send_buf()[..].into();
+        let packet = Packet::read(&mut b).unwrap();
         assert_eq!(packet, Packet::Choke);
     }
 
@@ -110,11 +153,8 @@ mod tests {
     fn parse_have() {
         let mut c = Connection::new();
         c.send_have(2);
-        let mut b = &c.get_send_buf()[..];
-
-        let len = b.get_u32();
-        let id = b.get_u8();
-        let packet = Packet::read(len, id, &mut b);
+        let mut b = c.get_send_buf()[..].into();
+        let packet = Packet::read(&mut b).unwrap();
 
         assert_eq!(packet, Packet::Have { index: 2 });
     }
@@ -123,11 +163,8 @@ mod tests {
     fn parse_extended() {
         let mut c = Connection::new();
         c.send_extended(0, &[]);
-        let mut b = &c.get_send_buf()[..];
-
-        let len = b.get_u32();
-        let id = b.get_u8();
-        let packet = Packet::read(len, id, &mut b);
+        let mut b = c.get_send_buf()[..].into();
+        let packet = Packet::read(&mut b).unwrap();
 
         assert_eq!(packet, Packet::Extended { len: 1 });
     }
@@ -136,11 +173,8 @@ mod tests {
     fn parse_bitfield() {
         let mut c = Connection::new();
         c.send_bitfield();
-        let mut b = &c.get_send_buf()[..];
-
-        let len = b.get_u32();
-        let id = b.get_u8();
-        let packet = Packet::read(len, id, &mut b);
+        let mut b = c.get_send_buf()[..].into();
+        let packet = Packet::read(&mut b).unwrap();
 
         assert_eq!(packet, Packet::Bitfield { len: 0 });
     }
